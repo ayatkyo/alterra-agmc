d641c2970cbdc3f67123b093cd647901c31a4de9
Hidayatullah <ayat.kyo@gmail.com>
2022-09-15 20:32:09 +0800

    chore: add search helper
    Signed-off-by: Hidayatullah <ayat.kyo@gmail.com>

---
+++ day-2/utils/utils.go
@@ -0,0 +1,12 @@
+package utils
+
+func Find[T any](collection []T, predicate func(T) bool) (T, bool) {
+	for _, item := range collection {
+		if predicate(item) {
+			return item, true
+		}
+	}
+
+	var result T
+	return result, false
+}
