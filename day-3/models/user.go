5fab9625f78d83b36c4834e741631af2ef883a24
Hidayatullah <ayat.kyo@gmail.com>
2022-09-21 20:30:34 +0800

    chore: add as response method for user model
    Signed-off-by: Hidayatullah <ayat.kyo@gmail.com>

--- day-3/models/user.go
+++ day-3/models/user.go
@@ -10,4 +10,16 @@ type User struct {
 	Username string `json:"username" form:"username"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password,omitempty" form:"password"`
+	Token    string `json:"token,omitempty" form:"token"`
+}
+
+func (u User) AsResponse() map[string]any {
+	return map[string]any{
+		"id":         u.ID,
+		"fullname":   u.Fullname,
+		"username":   u.Username,
+		"email":      u.Email,
+		"created_at": u.CreatedAt,
+		"updated_at": u.UpdatedAt,
+	}
 }
