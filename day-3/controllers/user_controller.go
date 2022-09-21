f19fdfd652678104c2d4f02bb2b3c44f59f7b34c
Hidayatullah <ayat.kyo@gmail.com>
2022-09-21 20:32:11 +0800

    chore(day 3): reimplement user
    Signed-off-by: Hidayatullah <ayat.kyo@gmail.com>

---
+++ day-3/controllers/user_controller.go
@@ -0,0 +1,148 @@
+package controllers
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/ayatkyo/alterra-agcm/day-3/lib/database"
+	"github.com/ayatkyo/alterra-agcm/day-3/models"
+	"github.com/ayatkyo/alterra-agcm/day-3/utils"
+	"github.com/labstack/echo/v4"
+)
+
+func UserGetAll(c echo.Context) error {
+	users, err := database.GetUsers()
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	// hide some information
+	list := []map[string]any{}
+	for _, user := range users.([]models.User) {
+		list = append(list, user.AsResponse())
+	}
+
+	return utils.ResponseSuccess(c, "Get all users", list)
+}
+
+func UserGetByID(c echo.Context) error {
+	// Parse id
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return utils.ResponseError(c, "Cannot parse user ID")
+	}
+
+	// Find user
+	user, err := database.GetUserByID(id)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	// Parse user
+	userRes, ok := user.(models.User)
+	if !ok {
+		return utils.ResponseError(c, "Cannot parse user")
+	}
+
+	return utils.ResponseSuccess(c, "Get user by ID", userRes.AsResponse())
+}
+
+func UserStore(c echo.Context) error {
+	// Get user data
+	user := models.User{}
+	err := c.Bind(&user)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	// Create password hash
+	passwordHash, err := utils.BcryptMake(user.Password)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	// Set password hash
+	user.Password = passwordHash
+
+	// Create user
+	newUser, err := database.CreateUser(user)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	// Parse user
+	userRes, ok := newUser.(models.User)
+	if !ok {
+		return utils.ResponseError(c, "Cannot parse user")
+	}
+
+	return utils.ResponseSuccess(c, "User created", userRes.AsResponse())
+}
+
+func UserUpdate(c echo.Context) error {
+	// Parse id
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return utils.ResponseError(c, "Cannot parse user ID")
+	}
+
+	// Get user input
+	userData := models.User{}
+	err = c.Bind(&userData)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	// Find user
+	found, err := database.GetUserByID(id)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	// parse user
+	user, ok := found.(models.User)
+	if !ok {
+		return utils.ResponseError(c, "Cannot parse user")
+	}
+
+	// Update data
+	user.Fullname = userData.Fullname
+	user.Email = userData.Email
+	user.Username = userData.Username
+
+	// Check password change
+	if userData.Password != "" {
+		// Create password hash
+		passwordHash, err := utils.BcryptMake(userData.Password)
+		if err != nil {
+			return utils.ResponseError(c, err.Error())
+		}
+
+		// Set password hash
+		user.Password = passwordHash
+	}
+
+	// Update in db
+	updatedUser, err := database.UpdateUser(user)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	return utils.ResponseSuccess(c, "User updated", updatedUser)
+}
+
+func UserDestroy(c echo.Context) error {
+	// Parse id
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return utils.ResponseError(c, "Cannot parse user ID")
+	}
+
+	// Delete user in db
+	err = database.DeleteUser(id)
+	if err != nil {
+		return utils.ResponseError(c, err.Error())
+	}
+
+	return utils.ResponseSuccess(c, fmt.Sprintf("Successfully delete user with id %d", id), nil)
+}
---
+++ day-3/lib/database/user.go
@@ -0,0 +1,66 @@
+package database
+
+import (
+	"errors"
+
+	"github.com/ayatkyo/alterra-agcm/day-3/config"
+	"github.com/ayatkyo/alterra-agcm/day-3/models"
+)
+
+func GetUsers() (any, error) {
+	var users []models.User
+
+	if e := config.DB.Find(&users).Error; e != nil {
+		return nil, e
+	}
+
+	return users, nil
+}
+
+func GetUserByID(ID int) (any, error) {
+	var user models.User
+
+	if e := config.DB.Where("id = ?", ID).First(&user).Error; e != nil {
+		return nil, e
+	}
+
+	return user, nil
+}
+
+func CreateUser(user models.User) (any, error) {
+	if e := config.DB.Create(&user).Error; e != nil {
+		return nil, e
+	}
+
+	return user, nil
+}
+
+func UpdateUser(user models.User) (any, error) {
+	if e := config.DB.Save(&user).Error; e != nil {
+		return nil, e
+	}
+
+	return user, nil
+}
+
+func DeleteUser(ID int) error {
+	var user models.User
+
+	//	Find user
+	e := config.DB.Where("id = ?", ID).First(&user).Error
+	if e != nil {
+		return e
+	}
+
+	if user.ID == 0 {
+		return errors.New("User not found")
+	}
+
+	//	Delete
+	e = config.DB.Delete(&user).Error
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
