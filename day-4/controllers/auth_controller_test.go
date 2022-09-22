efe3b7130238cc963cc4b7cf07c116311db8c3f9
Hidayatullah <ayat.kyo@gmail.com>
2022-09-23 03:34:29 +0800

    chore(day 4): change auth controller test
    Signed-off-by: Hidayatullah <ayat.kyo@gmail.com>

--- day-4/controllers/auth_controller_test.go
+++ day-4/controllers/auth_controller_test.go
@@ -6,68 +6,13 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ayatkyo/alterra-agcm/day-4/config"
-	"github.com/ayatkyo/alterra-agcm/day-4/models"
 	"github.com/ayatkyo/alterra-agcm/day-4/utils"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
-var (
-	e  *echo.Echo
-	DB *gorm.DB
-)
-
-func init() {
-	var err error
-
-	// Setup test database
-	DB, err = gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	// Migrate
-	DB.AutoMigrate(&models.User{})
-
-	// Replace DB
-	config.DB = DB
-
-	// Seed test data
-	pass, _ := utils.BcryptMake("rahasia")
-	var users = []models.User{
-		{
-			Fullname: "Hidayatullah",
-			Email:    "ayat@gmail.com",
-			Username: "ayat",
-			Password: pass,
-		},
-		{
-			Fullname: "Muhammad Al-Fath",
-			Email:    "alfath@gmail.com",
-			Username: "alfath",
-			Password: pass,
-		},
-		{
-			Fullname: "Siti Mawaddah",
-			Email:    "mawad@gmail.com",
-			Username: "mawad",
-			Password: pass,
-		},
-	}
-	DB.Create(&users)
-
-	// Setup echo
-	e = echo.New()
-	e.Validator = utils.EchoCustomValidator
-}
-
-func createContextWithBody(body string) (echo.Context, *httptest.ResponseRecorder) {
+func authTestContextWithBody(body string) (echo.Context, *httptest.ResponseRecorder) {
+	e, _ := utils.SetupTest()
 	req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(body))
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 
@@ -78,7 +23,7 @@ func createContextWithBody(body string) (echo.Context, *httptest.ResponseRecorde
 }
 
 func TestLoginSuccess(t *testing.T) {
-	c, res := createContextWithBody(`{
+	c, res := authTestContextWithBody(`{
 		"username": "ayat",
 		"password": "rahasia"
 	}`)
@@ -89,7 +34,7 @@ func TestLoginSuccess(t *testing.T) {
 }
 
 func TestLoginInvalid(t *testing.T) {
-	c, res := createContextWithBody(`{
+	c, res := authTestContextWithBody(`{
 		"username": "random",
 		"password": "random"
 	}`)
@@ -100,7 +45,7 @@ func TestLoginInvalid(t *testing.T) {
 }
 
 func TestLoginWrongPass(t *testing.T) {
-	c, res := createContextWithBody(`{
+	c, res := authTestContextWithBody(`{
 		"username": "ayat",
 		"password": "inisalah"
 	}`)
@@ -111,7 +56,7 @@ func TestLoginWrongPass(t *testing.T) {
 }
 
 func TestLoginValidation(t *testing.T) {
-	c, res := createContextWithBody(``)
+	c, res := authTestContextWithBody(``)
 
 	if assert.NoError(t, AuthLogin(c)) {
 		assert.Equal(t, http.StatusBadRequest, res.Code)
